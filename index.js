2e6176d5e6567cfcef7749fc3720ea70e2c1c9df
matriiixu <oani1_16@uni.worc.ac.uk>
2017-11-27 10:32:17 +0000

    Update1.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var watchID;
 var accelerometerOptions = { frequency: 2000 };  // Update every 2 seconds
-accelerometerOptions.frequency = 3000; //changed my mind - now 3 seconds
+
 
 
 //when the page is created...
@@ -56,8 +56,52 @@ function accelerometerError() {
 }
 
 function updateFreq(freq) {
-	//do something to update freq. here.
+	//do something to update freq. here
+    stopSensor();
+    
+    accelerometerOptions.frequency = freq; //changed my mind - now 3 seconds
+    
+    startSensor();
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
